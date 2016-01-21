8f30e4fffbaf71f6f5da2e9d8a08b6bc3b349cec
Datuman <kod5kod@users.noreply.github.com>
2016-01-21 11:27:32 -0500

    Update emailer.py

--- py2mail/emailer.py
+++ py2mail/emailer.py
@@ -1,15 +1,14 @@
 
 # -*- coding: utf-8 -*-
 """
-#### EMAILER v0.01 Beta #####
+#### py2mail v0.01 Beta #####
 Created on Thursday, October  29  10:02:04 2015
 @author: Lee Carlin
 lee@adtheorent.com
 
-The "emailer" module is a simple "smtplib" wrapper for sending emails.
+The "py2mail" module is a simple "smtplib" wrapper for sending emails.
 
-Required config files (\etc):
-* conf_file.ini (configurations)
+Required config dictionary
 
 """
 
@@ -19,39 +18,58 @@ import smtplib
 # Import the email modules we'll need
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
+from email.mime.application import MIMEApplication
+from os.path import basename
 # Import the connection to config file:
-import ConfigParser
 
-
-def emailer(mail_conf_path,mail_list,subject,email_body_html):
-
+def emailer(mail_conf,subject,email_body_html_file, files = None):
+    """
+    This function is a wrapper to smtplib.
+    :param mail_configs: A dictionary with the following:
+                mail_conf = {
+                            'client':'your client',
+                            'my_mail' : 'youremail@yourmail',
+                            'my_pass' : 'your password',
+                            'mail_list' : 'first@mail,second@mail',
+                            }
+    :param subject: your email subject
+    :param email_body_html_file: an html file you'ld like to send
+    :param files: any attachements yould like to add
+    :return: None. an email should be sent.
+    """
     ## Initializing the configs: ##################################
-    configs = ConfigParser.ConfigParser()
-    configs.read(mail_conf_path)
-    # Saving input var from the config file as a dictionary:
-    mail_conf = dict(configs.items('mail_conf'))
     # declaring variables from input_var dictionary for ease of calling:
     my_mail  = mail_conf['my_mail']
     my_pass  = mail_conf['my_pass']
-
-
+    mail_list = mail_conf['mail_list']
+    client = mail_conf['client']
     # Open a plain text file for reading.  For this , assume that
     # the text file contains only ASCII characters.
-    textfile = email_body_html
+    textfile = email_body_html_file
     # opening a connection to the file and saving the data to the msg instance
     fp = open(textfile, 'rb')
     msg = MIMEMultipart()
     msg.attach(MIMEText(fp.read(), 'html'))
     fp.close()
 
+    # Attaching files
+    for f in files or []:
+        with open(f, "rb") as fil:
+            msg.attach(MIMEApplication(
+                fil.read(),
+                Content_Disposition='attachment; filename="%s"' % basename(f),
+                Name=basename(f)
+            ))
+
+
     #populating the required fields to send the email:
     msg['Subject'] = subject
     msg['From'] = my_mail
     msg['To'] = mail_list
     # Send the message via your  own SMTP server
-    server = smtplib.SMTP('smtp.gmail.com')
+    server = smtplib.SMTP(client)
     server.ehlo()
     server.starttls()
     server.login(my_mail, my_pass)
-    server.sendmail(msg['From'], mail_conf['mail_list'].split(','), msg.as_string())
+    server.sendmail(msg['From'], mail_list.split(','), msg.as_string())
     server.quit()
